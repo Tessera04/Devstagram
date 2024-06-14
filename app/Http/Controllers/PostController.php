9a2ffbdd83847796baeced263e883a9dbe73929f
Matias <matute_11@hotmail.com.ar>
2024-06-14 16:39:41 -0300

    Eloquent database relationship

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,12 +25,12 @@ class PostController extends Controller
             'imagen'=>'required'
         ]);
 
-        Post::create([
+        /*Post::create([
             'titulo' => $request->titulo,
             'descripcion' => $request->descripcion,
             'imagen' => $request->imagen,
             'user_id' => auth()->user()->id
-        ]);
+        ]);*/
 
             //Otra forma de crear registros
             // $post = new Post;
@@ -39,6 +39,14 @@ class PostController extends Controller
             // $post->imagen = request->imagen;
             // $post->user_id = auth()->user()->id;
             // $post->save();
+
+        //Esto hace lo mismo que los dos de arriba, es a gusto de uno como subimos los datos a la base
+        $request->user()->posts()->create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
